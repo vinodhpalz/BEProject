cdee799d0f032a12664e4758b455a4a1b4e3d1c4
user <user@vinodh>
2018-10-07 12:47:14 +0530

    Back End Project Added

---
+++ BEProject/src/main/java/model/DBConfig.java
@@ -0,0 +1,22 @@
+package model;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DBConfig {
+
+	private Configuration cfg;
+	private SessionFactory sf;
+	private Session sess;
+	
+	public DBConfig()
+	{
+		cfg = new Configuration();
+		sf = cfg.configure("Hibernate.cfg.xml").addAnnotatedClass(Product.class).buildSessionFactory();
+	}
+	public Session getSess()
+	{
+		sess = sf.getCurrentSession();
+		return sess;
+	}
+}
---
+++ BEProject/src/main/java/model/ProductDAO.java
@@ -0,0 +1,50 @@
+package model;
+import java.util.List;
+
+import org.hibernate.Session;
+
+public class ProductDAO {
+
+	private DBConfig db;
+	private Session sess;
+	public ProductDAO()
+	{
+		db = new DBConfig();
+	}
+	
+	public void insertProduct(Product p)
+	{
+		try
+		{
+			sess = db.getSess();
+			sess.beginTransaction();
+			sess.save(p);
+			sess.getTransaction().commit();
+			
+		}catch(Exception ex)
+		{
+			sess.getTransaction().rollback();
+			ex.printStackTrace();
+		}
+	}
+	
+	public List<Product> getProducts()
+	{
+		List<Product> lp = null;
+		try
+		{
+			sess = db.getSess();
+			sess.beginTransaction();
+			lp = sess.createQuery("FROM Product",Product.class).getResultList();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return lp;
+	}
+}
+	
+	
+	
+	
+	
